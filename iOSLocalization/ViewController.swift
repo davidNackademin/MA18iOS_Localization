c5b2f57aef0f6650c2bd5c749a468b6170e7c740
David Svensson <david@Admins-MacBook-Pro-3.local>
2019-01-25 11:56:57 +0100

    added pics

--- iOSLocalization/ViewController.swift
+++ iOSLocalization/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var helloLabel: UILabel!
     
+    //var namesOfInts = [Int: String]()
+    var airports : [String :String] = [ "ARN": "Arlanda", "LAX": "Los Angeles", "LHR": "Heatrow", "BKK": "Bangkok" ]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(airports["LHR"])
         // Do any additional setup after loading the view, typically from a nib.
     }
 
