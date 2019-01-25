ec3fe0a38a18dd7c45155f1e2d9bd6c441991459
David Svensson <david@Admins-MacBook-Pro-3.local>
2019-01-25 09:31:54 +0100

    First commit

---
+++ iOSLocalization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLocalization
+//
+//  Created by David Svensson on 2019-01-25.
+//  Copyright © 2019 David Svensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
