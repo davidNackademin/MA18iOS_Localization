cd7fe94ad444a19fbcdef8f9dc086aa854b97721
David Svensson <david@Admins-MacBook-Pro-3.local>
2019-01-25 10:57:32 +0100

    localized strings

--- iOSLocalization/ViewController.swift
+++ iOSLocalization/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        helloLabel.text = NSLocalizedString("button_pressed", comment: "")
+    
+    
+    }
+    
 }
 
